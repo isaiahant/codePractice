84e1247014fac2247db01e78d0d43c03f534cb60
Isaiah Anthony <isaiah.anthony99@gmail.com>
2020-12-30 14:41:31 -0800

    disregard this

---
+++ app.js
@@ -0,0 +1,61 @@
+const fs = require('fs')
+// // let op = process.argv[2]
+// // let a = parseInt(process.argv[3])
+// // let b = parseInt(process.argv[4])
+
+// // switch (op) {
+// //   case 'add':
+// //     console.log(a + b);
+// //   break;
+
+// //   case 'subtract':
+// //     console.log(a - b);
+// //   break;
+
+// //   case 'multiply':
+// //     console.log(a * b);
+// //   break;
+
+// //   case 'divide':
+// //     console.log(a / b);
+// //   break;
+// // }
+
+// // console.log(fs);
+
+// // fs.writeFile('example.txt','hello world', (err) =>{
+// //   if (err) {console.log(err);}
+// // })
+
+// // fs.writeFile('arr.js', '[1,2,3,4,5]', (err) =>{
+// //   if (err) {console.log(err);}
+// // })
+// let dog = {
+//   name: 'Buddy',
+//   age: 7,
+//   breed: 'Jack Russel Terrier'
+// }
+
+// // fs.writeFile('dog.html', '', (err) => {
+// //   if (err) { console.log(err) }
+// // })
+
+// // fs.appendFile('dog.html', `
+// // <h1>${dog.name}</h1>
+// // <h2>${dog.age}</h2>
+// // <h3>${dog.breed}</h2>
+// // `, (err) => {
+// //   if (err) { console.log(err) }
+// // })
+
+// fs.readFile('dog.html', 'utf8', (err, html) => {
+//   if (err) {console.log(err)}
+//   console.log(html)
+// })
+
+
+// let person = require('./person.js')
+
+// console.log(person)
+
+
---
+++ person.js
@@ -0,0 +1,8 @@
+let person = {
+  name: 'john',
+  age: '24',
+  email: 'john@gmail',
+  address: '123 main st'
+}
+
+module.exports = person
